1b204f6f23e4777292aaf201effc5aa3a27d8e24
Jinjiang <zhaojinjiang@me.com>
2017-07-10 15:25:20 +0800

    Support cli

---
+++ bin/cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const cli = require('cli')
+const Bao = require('../src/bao')
+
+cli.parse({
+  output: ['o', 'The output file path.', 'file'],
+  watch: ['w', 'Watch file changes.', 'bool', false]
+})
+
+cli.main(function (args, options) {
+  const input = args[0]
+  const { output, watch } = options
+
+  if (input) {
+    const bao = new Bao({
+      input, output
+    })
+    bao.build(watch)
+  }
+})
