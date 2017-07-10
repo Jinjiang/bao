ee2c55c1388caf109eb8222d989152e61e62e140
Jinjiang <zhaojinjiang@me.com>
2017-07-10 18:02:14 +0800

    Supported jsx.

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ function genBasicConfig () {
     module: {
       rules: [
         { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['env']}}, exclude: /(node_modules|bower_components)/ },
+        { test: /\.jsx$/, use: { loader: 'babel-loader', options: { presets: ['env', 'react']}}},
         { test: /\.json$/, use: 'json-loader' },
         { test: /\.css$/, use: [{ loader: "style-loader" }, { loader: "css-loader" }]},
         { test: /\.vue$/, use: 'vue-loader' }
