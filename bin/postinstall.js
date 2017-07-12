0c49434381ac8adfc70a38015f587183497df8e2
Jinjiang <zhaojinjiang@me.com>
2017-07-12 20:22:58 +0800

    Added postinstall script to solve deps problem.

---
+++ bin/postinstall.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const lnk = require('lnk')
+const fs = require('fs')
+
+// if webpack deps not at the top node_modules
+// then link them from this sub node_modules
+
+function checkModule (name) {}
+
+function linkModule (name) {}
