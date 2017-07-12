e313a81b68c6438fc722bf9e43941a55586ef580
Jinjiang <zhaojinjiang@me.com>
2017-07-13 02:19:05 +0800

    Tried to solve bug in npx case.

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,11 +1,47 @@
 #!/usr/bin/env node
 
-const lnk = require('lnk')
 const fs = require('fs')
+const path = require('path')
 
-// if webpack deps not at the top node_modules
-// then link them from this sub node_modules
+const lnk = require('lnk')
 
-function checkModule (name) {}
+const NOT_FOUND = -1
+const IN_CURRENT_DIR = 0
+const AT_TOP_DIR = 1
+const IN_BOTH = 2
 
-function linkModule (name) {}
+const currentDir = path.resolve(__dirname, '..', 'node_modules')
+const topDir = path.resolve(__dirname, '..', '..')
+console.log('CURRENT_DIR', currentDir)
+console.log('TOP_DIR', topDir)
+
+function findModule (name) {
+  if (fs.existsSync(path.join(currentDir, name))) {
+    if (fs.existsSync(path.join(topDir, name))) {
+      return IN_BOTH
+    }
+    return IN_CURRENT_DIR
+  } else if (fs.existsSync(path.join(topDir, name))) {
+    return AT_TOP_DIR
+  } else {
+    return NOT_FOUND
+  }
+}
+
+function linkModule (name) {
+  console.log('link', name, path.join(currentDir, name), topDir)
+  lnk.sync(path.join(currentDir, name), topDir)
+}
+
+function checkModule (name) {
+  if (findModule(name) === IN_CURRENT_DIR) {
+    console.log('IN_CURRENT_DIR', name)
+    linkModule(name)
+  } else {
+    console.log('NOTHING_HAPPEN', name)
+  }
+}
+
+// checkModule('vue-loader')
+checkModule('babel-preset-env')
+checkModule('babel-preset-react')
