7f08d3c6ffc1a1610eea29b1b867cdeff89d8ba4
Jinjiang <zhaojinjiang@me.com>
2017-07-13 13:48:42 +0800

    Fixed module path problem.

--- src/config.js
+++ src/config.js
@@ -14,14 +14,28 @@ function genBasicConfig () {
     context: path.resolve(__dirname, '../'),
     module: {
       rules: [
-        { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['env']}}, exclude: /(node_modules|bower_components)/ },
-        { test: /\.jsx$/, use: { loader: 'babel-loader', options: { presets: ['env', 'react']}}},
+        { test: /\.js$/, use: {
+          loader: 'babel-loader',
+          options: { presets: resolveModulePath(['babel-preset-env'])}},
+          exclude: /(node_modules|bower_components)/ },
+        { test: /\.jsx$/, use: {
+          loader: 'babel-loader',
+          options: { presets: resolveModulePath(['babel-preset-env', 'babel-preset-react'])}}},
         { test: /\.json$/, use: 'json-loader' },
         { test: /\.css$/, use: [{ loader: "style-loader" }, { loader: "css-loader" }]},
         { test: /\.vue$/, use: 'vue-loader' }
       ]
     }
   }
+}
+
+function resolveModulePath (names) {
+  if (typeof names === 'string') {
+    return resolveModulePath([names])
+  }
+  const result = names.map(require.resolve)
+  console.log(names, result)
+  return result
 }
 
 exports.genBasicConfig = genBasicConfig
