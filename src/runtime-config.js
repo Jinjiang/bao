8e8a4d57282e9428c35417401d11647baab788a0
Jinjiang <zhaojinjiang@me.com>
2017-07-17 17:45:07 +0800

    Config shared common chunks.

--- src/runtime-config.js
+++ src/runtime-config.js
@@ -6,6 +6,11 @@
 const fs = require('fs')
 const path = require('path')
 
+/**
+ * `.dabao.alias.json`:
+ * Key-value pairs about alias name and the real file path.
+ */
+
 function readAliasRc () {
   if (!fs.existsSync('./.dabao.alias.json')) {
     return {}
@@ -34,4 +39,45 @@ function getAliasConfig () {
   return alias
 }
 
+/**
+ * '.dabao.shared.json':
+ * Key-value pairs about common chunk filenames and lists of shared module names.
+ */
+
+function readCommonChunksRc () {
+  if (!fs.existsSync('./.dabao.shared.json')) {
+    return {}
+  }
+  try {
+    const content = fs.readFileSync('./.dabao.shared.json')
+    return JSON.parse(content)
+  } catch (e) {
+    console.warn('[Warning] File parsing error: .dabao.shared.json')
+    console.warn(e)
+    return {}
+  }
+}
+
+function getCommonChunksConfig () {
+  const chunks = readCommonChunksRc()
+  for (const filename in chunks) {
+    const moduleNames = chunks[filename]
+    if (typeof moduleNames === 'string') {
+      const moduleName = moduleNames
+      chunks[filename] = [moduleName]
+    } else if (Array.isArray(moduleNames)) {
+      chunks[filename] = moduleNames.filter(moduleName => typeof moduleName === 'string')
+      if (chunks[filename].length === 0) {
+        console.warn(`[Warning] Shared parsing: no legal module name found in ${filename}`)
+        delete chunks[filename]
+      }
+    } else {
+      console.warn(`[Warning] Shared parsing: invalid config for ${filename}`)
+      delete chunks[filename]
+    }
+  }
+  return chunks
+}
+
 exports.getAliasConfig = getAliasConfig
+exports.getCommonChunksConfig = getCommonChunksConfig
